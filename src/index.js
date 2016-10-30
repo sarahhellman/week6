bb8dee89b224a0b93770ba11d5e692492eea9fad
sarahhellman <shellman2017@kellogg.northwestern.edu>
2016-10-29 22:48:09 -0500

    drop

---
+++ src/index.js
@@ -0,0 +1,65 @@
+// DO NOT MODIFY ANYTHING BETWEEN LINES 1-33. NO REALLY.
+// React
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// shuffles (randomizes an array)
+// e.g. myArray.shuffle()
+Array.prototype.shuffle = function() {
+  var currentIndex = this.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this[currentIndex];
+    this[currentIndex] = this[randomIndex];
+    this[randomIndex] = temporaryValue;
+  }
+  return this;
+}
+
+// returns a deck of cards
+// e.g. getDeck()
+window.getDeck = function() {
+  var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+  var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  var cards = [];
+  ranks.forEach(function(rank, index) {
+    suits.forEach(function(suit, index) {
+      cards.push(rank + "_of_" + suit);
+    });
+  });
+  return cards;
+}
+// END OF STUFF TO NOT MODIFY
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Welcome to the KIEI-924 Casino!</h1>
+        <div className="row">
+          <div className="col-sm-2">
+            <h1><img className="img-responsive" src={"http://golearntocode.com/images/cards/ace_of_hearts.png"} /></h1>
+          </div>
+          <div className="col-sm-2">
+            <h1><img className="img-responsive" src={"http://golearntocode.com/images/cards/ace_of_hearts.png"} /></h1>
+          </div>
+          <div className="col-sm-2">
+            <h1><img className="img-responsive" src={"http://golearntocode.com/images/cards/ace_of_hearts.png"} /></h1>
+          </div>
+          <div className="col-sm-2">
+            <h1><img className="img-responsive" src={"http://golearntocode.com/images/cards/ace_of_hearts.png"} /></h1>
+          </div>
+          <div className="col-sm-2">
+            <h1><img className="img-responsive" src={"http://golearntocode.com/images/cards/ace_of_hearts.png"} /></h1>
+          </div>
+          <div className="col-sm-2">
+            <h1><a href="#" className="btn btn-success">Deal</a></h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById("app"))
